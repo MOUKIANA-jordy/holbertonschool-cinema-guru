9ebef9ca89987c20a61a77b33fd1d70a9b18f662
MOUKIANA-jordy <jordymoukiana@gmail.com>
2026-04-28 21:46:17 +0200

    add App

--- cinema-guru/src/App.jsx
+++ cinema-guru/src/App.jsx
@@ -1,5 +1,48 @@
+import './App.css';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+
+// ⚠️ ces composants seront créés plus tard
+import Dashboard from './routes/Dashboard';
+import Authentication from './routes/Authentication';
+
 function App() {
-  return <div className="App"></div>;
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [userUsername, setUserUsername] = useState('');
+
+  useEffect(() => {
+    const token = localStorage.getItem('accessToken');
+
+    if (!token) return;
+
+    axios.post(
+      'http://localhost:8000/api/auth/',
+      {},
+      {
+        headers: {
+          Authorization: `Bearer ${token}`,
+        },
+      }
+    )
+      .then((res) => {
+        setIsLoggedIn(true);
+        setUserUsername(res.data.username);
+      })
+      .catch(() => {
+        setIsLoggedIn(false);
+        setUserUsername('');
+      });
+  }, []);
+
+  return (
+    <div className="App">
+      {isLoggedIn ? (
+        <Dashboard username={userUsername} />
+      ) : (
+        <Authentication />
+      )}
+    </div>
+  );
 }
 
 export default App;
