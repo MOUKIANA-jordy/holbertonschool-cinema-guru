c7e08187a4408f6b671d6178a58ede2d9452c867
MOUKIANA-jordy <jordymoukiana@gmail.com>
2026-04-28 21:52:38 +0200

    add files

--- cinema-guru/src/routes/auth/Authentication.jsx
+++ cinema-guru/src/routes/auth/Authentication.jsx
@@ -1,5 +1,7 @@
 import './auth.css';
 import { useState } from 'react';
+import axios from 'axios';
+
 import Login from './Login';
 import Register from './Register';
 import Button from '../../components/general/Button';
@@ -9,6 +11,29 @@ function Authentication({ setIsLoggedIn, setUserUsername }) {
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
 
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    const url = _switch
+      ? 'http://localhost:8000/api/auth/login'
+      : 'http://localhost:8000/api/auth/register';
+
+    axios.post(url, { username, password })
+      .then((res) => {
+        const token = res.data.accessToken;
+
+        // 🔐 stocker le token
+        localStorage.setItem('accessToken', token);
+
+        // ✅ mettre à jour l’état global
+        setUserUsername(username);
+        setIsLoggedIn(true);
+      })
+      .catch((err) => {
+        console.error('Auth error:', err);
+      });
+  };
+
   return (
     <div className="auth-container">
       <div className="auth-switch">
@@ -16,15 +41,14 @@ function Authentication({ setIsLoggedIn, setUserUsername }) {
         <Button label="Sign Up" onClick={() => setSwitch(false)} />
       </div>
 
-      <div className="auth-form">
+      {/* 🔥 IMPORTANT: form avec onSubmit */}
+      <form className="auth-form" onSubmit={handleSubmit}>
         {_switch ? (
           <Login
             username={username}
             password={password}
             setUsername={setUsername}
             setPassword={setPassword}
-            setIsLoggedIn={setIsLoggedIn}
-            setUserUsername={setUserUsername}
           />
         ) : (
           <Register
@@ -32,11 +56,9 @@ function Authentication({ setIsLoggedIn, setUserUsername }) {
             password={password}
             setUsername={setUsername}
             setPassword={setPassword}
-            setIsLoggedIn={setIsLoggedIn}
-            setUserUsername={setUserUsername}
           />
         )}
-      </div>
+      </form>
     </div>
   );
 }
